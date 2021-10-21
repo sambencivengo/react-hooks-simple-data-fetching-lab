3eeddc5c26d19a6566bab8f50556fae62b62ba20
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-20 20:29:37 -0400

    commit tree 6b637ac26cce3a0d3ae0fe3bbe68b70fca94f3ce

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
+import { useState, useEffect } from 'react';
+
 // create your App component here
+
+const App = () => {
+	const url = 'https://dog.ceo/api/breeds/image/random';
+	const [dog, setDog] = useState('');
+	useEffect(() => {
+		fetch(url)
+			.then((r) => r.json())
+			.then((data) => {
+				setDog(data.message);
+				console.log(data.message);
+			});
+	}, []);
+
+	const renderDog = () => {
+		return <img src={dog} alt="A Random Dog" />;
+	};
+
+	return <>{dog.length < 2 ? <p>Loading...</p> : renderDog()}</>;
+};
+
+export default App;
